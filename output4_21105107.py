de1ff057d6e76fe19bc5596d1547c428a73ecf1b
Manav332003 <97628752+Manav332003@users.noreply.github.com>
2022-02-27 20:06:12 +0530

    Add files via upload

---
+++ output4_21105107.py
@@ -0,0 +1,77 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\Users\Manav Goyal\OneDrive\Desktop\assignment4_21105107.txt ===
+Question 1
+Problem based on tower of hanoi containing 3 Disks
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+End of question
+
+Question 2
+Pascal's Triangle using Recursion
+Enter number:8
+        
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+ 1 6 15 20 15 6 1 
+1 7 21 35 35 21 7 1 
+Pascal's Triangle using iteration
+Enter the number of rows :8
+         1 
+        1 1 
+       1 2 1 
+      1 3 3 1 
+     1 4 6 4 1 
+    1 5 10 10 5 1 
+   1 6 15 20 15 6 1 
+  1 7 21 35 35 21 7 1
+
+  
+Question 3
+Enter the value of num_1:45
+Enter the value of num_2:4
+Part a):Checking if the quotient and remainder is a callable value or not
+False
+False
+Part b):Checking if value of quotient or remainder if zero
+Values are non zero
+[11, 1]
+Part c):Filtering the list_1
+Filtered Values: (11, 5, 6)
+Part d):Conversion of list into set
+Set_1 {1, 4, 5, 6, 11}
+Part e):Making set immutable
+Immutable set: frozenset({1, 4, 5, 6, 11})
+Part f):Calculaing Max value and hash value
+Max Value: 11
+Hash Value: 7838926217815587121
+
+
+Question 4
+Object is created
+The name of student is Manav Goyal and roll number is 21105107
+Object is Destroyed
+
+
+Question5
+The updated salary of Mehak is 70000
+The records of Viren are deleted
+
+
+Question 6
+Enter any word:naman
+Enter any new word using the same alphabets in word to test your friendship:manan
+Does the word makes any sense?(y/Y or n/N)y
+The Friends have passed the test successfully
